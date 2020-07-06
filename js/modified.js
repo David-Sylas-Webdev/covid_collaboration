c429a394c07efc17cd79d2b765f2ec0c8bcf120d
DavidMaccarthy <davidmaccarthy123@gmail.com>
2020-07-06 16:33:06 +0000

    added the covid status functionality

---
+++ js/modified.js
@@ -0,0 +1,111 @@
+const getCountry = function(){
+    
+    return new Promise((resolve, reject)=>{
+
+        let req = new XMLHttpRequest();
+
+        req.overrideMimeType("application/json");
+
+        req.open('GET', 'https://ipapi.co/json/', true);
+
+        req.onload  = function() {
+
+            if(this.status === 200){
+
+            resolve(JSON.parse(req.responseText).country_name);
+
+        }
+
+        else{
+            reject(new Error("Country was not obtained..."));
+        }
+
+        };
+         
+        req.send(null);
+
+    });
+};
+
+getCountry()
+            .then(function(country){
+                return country;
+            })
+            .then(
+
+                function(country){
+
+                    
+                       
+                        let xhr = new XMLHttpRequest();
+               
+                        let result, totalCases, recovered, deaths, updateDate,  obtained;
+               
+                        xhr.withCredentials = true;
+               
+                        xhr.open("GET", "https://covid-193.p.rapidapi.com/statistics",true);
+               
+                        xhr.setRequestHeader("x-rapidapi-host", "covid-193.p.rapidapi.com");
+               
+                        xhr.setRequestHeader("x-rapidapi-key", "d83b8be19emsh87b08ffd205652fp183ee4jsn6365020be2aa");
+               
+                        xhr.send(null);   
+                        
+                        xhr.onload = function(){
+               
+                           if(this.status = 200){
+                               obtained  = JSON.parse(this.responseText);
+               
+                               console.log(obtained);
+               
+                           for(let i=0; i<obtained.results; i++){
+                               
+               
+               
+                                if(country == obtained.response[i].country){
+               
+                                   result = obtained.response[i];
+               
+                                   console.log(obtained.response[i]);
+               
+                                   totalCases  = document.querySelector(".cases h1");
+                                   
+                                   totalCases.innerHTML = result.cases.active;
+                                   
+                                   recovered  = document.querySelector(".recovered h1");
+                                   
+                                   recovered.innerHTML = result.cases.recovered;
+                                   
+                                   deaths = document.querySelector(".deaths h1");
+                                   
+                                   deaths.innerHTML = result.deaths.total;
+               
+                                   
+               
+                                   
+                                }
+                               
+                             }
+                       
+                           }
+               
+                           else if (this.status == 403){
+                             throw new Error("Update resource is restricted...");
+                           }
+               
+                           else if(this.status == 404){
+                              throw new Error("Update resource is not found...");
+                           }
+               
+                        }
+                  
+                     
+               
+                    }
+               
+               
+
+            )
+            .catch(function(error){
+                console.log(error.message);
+            });
